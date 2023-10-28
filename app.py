110360ada9ce6ebbbf53e5e6f503a332e494e2af
Jovan Josafat <jovan.josafat@gmail.com>
2023-10-28 16:09:48 +0700

    update nih

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask , request, make_response, jsonify
+from flask import Flask , request, make_response, jsonify, send_file
 from flask_restful import Resource, Api
 from flask_cors import CORS
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
-from models import AuthModel
+
 from functools import wraps
-import jwt
-import os
-import datetime
+import jwt, os, datetime, werkzeug
+import numpy as np
+import cv2
+
+
+from models import AuthModel
 
 app = Flask(__name__)
 api = Api(app)
@@ -90,10 +93,275 @@ class ProtectedLoginJWT(Resource):
     def protected(self, data):
         return jsonify({'message': 'This is a protected endpoint', 'user': data['username']})
 
+# class GetFileList(Resource):
+#     # @token_requried
+#     def get_file_list():
+#         file_directory = './uploadedvideo/'
+#         file_list = []
+
+#         for filename in os.listdir(file_directory):
+#             file_path = os.path.join(file_directory, filename)
+#             file_info = {
+#                 'filename': filename,
+#                 'size': os.path.getsize(file_path),
+#                 'type': filename.split('.')[-1]  # Get the file type/extension
+#             }
+#             file_list.append(file_info)
+
+#         return jsonify(file_list)
+
+# class GetFileName(Resource):
+#     # @token_requried
+#     def get_file(filename):
+#         file_path = './uploadedvideo/'
+#         return send_file(os.path.join(file_path, filename))
+
+class UploadVideo(Resource):
+    # @token_requried
+    def upload_video():
+        if(request.method == "POST"):
+            videofile = request.files['image']
+            filename = werkzeug.utils.secure_filename(videofile.filename)
+            print("\nReceived image File name : " + videofile.filename)
+            videofile.save("./uploadedvideo/" + filename)
+            return jsonify({
+                "message" : "file uploaded successfully"
+            })
+
+
+#Preprocessing and Segmentation
+class Preprocessing(Resource):
+    def video2frames(self, video):
+        rawImages = {}
+        output_dir = 'frames'
+        os.makedirs(output_dir, exist_ok=True)
+        cap = cv2.VideoCapture(video)
+        target_frames = 10
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        frame_skip = max(total_frames // target_frames, 1)
+
+        if total_frames <= target_frames:
+            frame_skip = 1
+        else:
+            frame_skip = total_frames // target_frames
+        frame_count = 0
+        frame_index = 0
+        while frame_index < target_frames:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            if frame_count % frame_skip == 0:
+                rawImages[frame_count] = frame
+                output_image_path = os.path.join(output_dir, f'frame_{frame_count:04d}.png')
+                cv2.imwrite(output_image_path, frame)
+                frame_index += 1
+            frame_count += 1
+        cap.release()
+        return rawImages
+
+    def median_filter(self, rawImages):
+        median_filtered_images = {}
+        output_dir = 'medianfiltered'
+        os.makedirs(output_dir, exist_ok=True)
+        for frame_count, image in rawImages.items():
+            res = np.copy(image)
+            kernelsize = 27
+            res = cv2.medianBlur(image, kernelsize)
+            median_filtered_images[frame_count] = res
+            output_path = os.path.join(output_dir, f'frame_{frame_count:04d}.png')
+            cv2.imwrite(output_path, res)
+        return median_filtered_images
+
+    def high_boost_filter(self, source, lpf, kons):
+        hbfImages = {}
+        output_dir = 'highboost'
+        os.makedirs(output_dir, exist_ok=True)
+        for framecount, (image, lpf) in enumerate(zip(source.items(), lpf.items())):
+            #tupple image, lpf index ke 1 menyimpan bytes image sedangkan 0 menyimpan framecount, sehingga yang diakses disnini addalah index 1
+            res = np.copy(image[1])
+            #operasi manual highboost
+            for i in range(image[1].shape[0]):
+                for j in range(image[1].shape[1]):
+                    lpf_rgb = lpf[1][i, j]
+                    # src_rgb = image[1][i, j]
+
+                    for k in range(3):  # 3 channels (B, G, R)
+                        # val = kons * src_rgb[k] - lpf_rgb[k]
+                        val = kons * lpf_rgb[k]
+                        val = min(max(val, 0), 255)
+                        res[i, j, k] = val
+            res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+            hbfImages[framecount] = res
+            output_path = os.path.join(output_dir, f'frame_{framecount:04d}.png')
+            cv2.imwrite(output_path, res)
+        return hbfImages
+
+    def morph(self, source):
+        morphImages = {}
+        output_dir = 'morphology'
+        os.makedirs(output_dir, exist_ok=True)
+        for framecount, image in source.items():
+            res = np.copy(image)
+            ellipse = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (12, 12), (3,3))
+            res = cv2.morphologyEx(image, cv2.MORPH_OPEN, ellipse)
+            res = cv2.morphologyEx(res, cv2.MORPH_CLOSE, ellipse)
+            morphImages[framecount] = res
+            output_path = os.path.join(output_dir, f'frame_{framecount:04d}.png')
+            cv2.imwrite(output_path, res)
+        return morphImages
+
+    def thresholding(self, source):
+        thresholded = {}
+        output_dir = 'thresholding'
+        os.makedirs(output_dir, exist_ok=True)
+        for framecount, image in source.items():
+            res = np.copy(image)
+            _, res = cv2.threshold(image, 50, 255, cv2.THRESH_BINARY) #original at 90
+            thresholded[framecount] = res
+            output_path = os.path.join(output_dir, f'frame_{framecount:04d}.png')
+            cv2.imwrite(output_path, res)
+        return thresholded
+
+    def canny(self, source):
+        cannyFiltered = {}
+        output_dir = 'canny'
+        os.makedirs(output_dir, exist_ok=True)
+        for framecount, image in source.items():
+            res = image.copy()
+            res = cv2.Canny(image, 0, 255, 3)
+            cannyFiltered[framecount] = res
+            output_path = os.path.join(output_dir, f'frame_{framecount:04d}.png')
+            cv2.imwrite(output_path, res)
+        return cannyFiltered
+
+    def region_filter(self, source):
+        regionFiltered = {}
+        output_dir = 'region'
+        os.makedirs(output_dir, exist_ok=True)
+        for framecount, image in source.items():
+            contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            res = np.zeros_like(image)
+            output_path = os.path.join(output_dir, f'frame_{framecount:04d}.png')
+            for i in range(len(contours)):
+                if len(contours[i]) > self.R:
+                    cv2.drawContours(res, contours, i, (255, 0, 0), 1)
+                    regionFiltered[framecount] = res
+                    cv2.imwrite(output_path, res)
+        return regionFiltered
+
+
+    def coLinear(self, source):
+        colinearFiltered = {}
+        output_dir = 'colinear'
+        os.makedirs(output_dir, exist_ok=True)
+        for framecount, image in source.items():
+            contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            res = np.zeros_like(image)
+            output_path = os.path.join(output_dir, f'frame_{framecount:04d}.png')
+            data = [0] * len(contours)
+            idk = 0
+            for i, contour in enumerate(contours):
+                if len(contour) > self.R * 2:
+                    pt = contour[i][len(contour[i]) // 4]
+                    self.CCX[idk] = pt[0]
+                    self.CCY[idk] = pt[1]
+                    data[idk] = 0
+                else:
+                    self.CCX[idk] = 0
+                    self.CCY[idk] = 0
+                    data[idk] = 1
+                idk += 1
+            # Intersection line evaluation
+            for i in range(len(contours)):
+                for j in range(len(contours)):
+                    if i == j: continue
+                    out = 0
+                    for k in range(len(contours[i]) // 2):
+                        pt1 = contours[i][k][0]
+                        pt2 = contours[i][k + 2][0]
+                        out = self.intersectionLine(self.X1, self.Y1, self.CCX[j], self.CCY[j], pt1[0], pt1[1], pt2[0], pt2[1])
+                        if out == 1:
+                            if (abs(self.CCX[j] - pt1[0]) < 2) and (abs(self.CCY[j] - pt1[1]) < 2):
+                                data[j] = 0
+                            else:
+                                data[j] = 1
+
+            for i in range(len(contours)):
+                if data[i] == 0:
+                    cv2.drawContours(res, contours, i, (255, 255, 255), 1, lineType=8, hierarchy=hierarchy, maxLevel=0, offset=(0, 0))
+                    colinearFiltered[framecount] = res
+                    cv2.imwrite(output_path, res)
+        return colinearFiltered
+
+    def straightLine(self, x1, y1, x2, y2):
+        x = x1 - x2
+        if x == 0:
+            m = 1e6
+        else:
+            m = (y1 - y2) / x
+        b = y1 - m * x1
+        return m, b
+
+    def intersectionLine(self, x1, y1, x2, y2, x3, y3, x4, y4):
+        m1, c1 = self.straightLine(x1, y1, x2, y2)
+        m2, c2 = self.straightLine(x3, y3, x4, y4)
+
+        if m1 == m2:
+            return 0
+
+        xp = (c2 - c1) / (m1 - m2)
+        yp = m1 * xp + c1
+
+        if (x1 == x2) and ((xp - x3) * (xp - x4) < 0) and ((yp - y1) * (yp - y2) < 0):
+            return 1
+        if (x3 == x4) and ((xp - x1) * (xp - x2) < 0) and ((yp - y3) * (yp - y4) < 0):
+            return 1
+
+        if ((xp - x1) * (xp - x2) < 0) and ((xp - x3) * (xp - x4) < 0):
+            return 1
+        else:
+            return 0
+
+
+
+
+
+    def post(self):
+        #variable konstan
+        self.R = 71 #radius
+        self.X1, self.Y1 = 0, 0 #centerpoint
+        self.CCX, self.CCY = [0] * 100, [0] * 100
+        rawImages = {}
+        res ={}
+        if(request.method == "POST"):
+            videofile = request.files['video']
+            rawVideo = werkzeug.utils.secure_filename(videofile.filename)
+            print("\nReceived image File name : " + videofile.filename)
+            print(videofile)
+        rawImages = self.video2frames(rawVideo)
+        res = self.median_filter(rawImages)
+        res = self.high_boost_filter(rawImages, res, 1.5)
+        res = self.morph(res)
+        res = self.thresholding(res)
+        res = self.canny(res)
+        res = self.region_filter(res)
+        #ambil 1 frame untuk menympan nilai center point
+        height, width = res[0].shape
+        self.X1, self.Y1 = (width // 2), (height // 2)
+        res = self.coLinear(res)
+
+        
+
+
 
 api.add_resource(RegisterUser, "/register", methods = ["POST"])
 api.add_resource(LoginUser, "/login", methods = ["POST"])
 api.add_resource(ProtectedLoginJWT, "/protected", methods=["GET"])
+#api.add_resource(GetFileName, "/get_file/<filename>", methods=["GET"])
+#api.add_resource(GetFileList, "/get_file_list", methods=["GET"])
+api.add_resource(UploadVideo, "/upload", methods=["POST"])
+api.add_resource(Preprocessing, "/preprocessing", methods=["POST"])
 api.add_resource(HelloWorld, "/")
 
 if __name__ == '__main__':
---
+++ highboost.py
@@ -0,0 +1,149 @@
+import cv2
+import os
+import numpy as np
+
+def high_boost_filter(image, lpf_image, kons):
+    res = np.copy(image)
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            lpf_rgb = lpf_image[i, j]
+            src_rgb = image[i, j]
+
+            for k in range(3):  # 3 channels (B, G, R)
+                # val = kons * src_rgb[k] - lpf_rgb[k]
+                val = kons * lpf_rgb[k]
+                val = min(max(val, 0), 255)
+                res[i, j, k] = val
+    res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+    return res
+
+def morph(source):
+    res = np.copy(source)
+    ellipse = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (12, 12), (3,3))
+    res = cv2.morphologyEx(source, cv2.MORPH_OPEN, ellipse)
+    res = cv2.morphologyEx(res, cv2.MORPH_CLOSE, ellipse)
+    return res
+
+
+def thresholding(source):
+    res = np.copy(source)
+    _, res = cv2.threshold(source, 50, 255, cv2.THRESH_BINARY)
+    return res
+
+def canny(source):
+    res = np.copy(source)
+    res = cv2.Canny(source, 0, 255, 3)
+    return res
+
+def region_filter(source):
+    contours, hierarchy = cv2.findContours(source, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    res = np.zeros_like(source)
+    for i in range(len(contours)):
+        if len(contours[i]) > R:
+            cv2.drawContours(res, contours, i, (255, 0, 0), 1, lineType=8, hierarchy=hierarchy, maxLevel=0, offset=(0, 0))
+           # cv2.drawContours(res, contours, i, (255, 0, 0), 1)
+    return res
+
+def coLinear(source):
+    # Find contours in the input image
+    contours, hierarchy = cv2.findContours(source, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    res = np.zeros_like(source)
+    height, width = source.shape
+    print(width // 2)
+    print(height // 2)
+    # data = [0] * len(contours)
+    data = [0] * len(contours)
+
+    idk = 0
+    for i, contour in enumerate(contours):
+        if len(contour) > R * 2:
+            pt = contour[i][len(contour[i]) // 4]
+            CCX[idk] = pt[0]
+            CCY[idk] = pt[1]
+            data[idk] = 0
+        else:
+            CCX[idk] = 0
+            CCY[idk] = 0
+            data[idk] = 1
+        idk += 1
+    # Intersection line evaluation
+    for i in range(len(contours)):
+        for j in range(len(contours)):
+            if i == j: continue
+            out = 0
+            for k in range(len(contours[i]) // 2):
+                pt1 = contours[i][k][0]
+                pt2 = contours[i][k + 2][0]
+                out = intersectionLine(X1, Y1, CCX[j], CCY[j], pt1[0], pt1[1], pt2[0], pt2[1])
+                if out == 1:
+                    if (abs(CCX[j] - pt1[0]) < 2) and (abs(CCY[j] - pt1[1]) < 2):
+                        data[j] = 0
+                    else:
+                        data[j] = 1
+
+    for i in range(len(contours)):
+        if data[i] == 0:
+            cv2.drawContours(res, contours, i, (255, 255, 255), 1, lineType=8, hierarchy=hierarchy, maxLevel=0, offset=(0, 0))
+
+    return res
+def intersectionLine(x1, y1, x2, y2, x3, y3, x4, y4):
+    m1, c1 = straightLine(x1, y1, x2, y2)
+    m2, c2 = straightLine(x3, y3, x4, y4)
+
+    if m1 == m2:
+        return 0
+
+    xp = (c2 - c1) / (m1 - m2)
+    yp = m1 * xp + c1
+
+    if (x1 == x2) and ((xp - x3) * (xp - x4) < 0) and ((yp - y1) * (yp - y2) < 0):
+        return 1
+    if (x3 == x4) and ((xp - x1) * (xp - x2) < 0) and ((yp - y3) * (yp - y4) < 0):
+        return 1
+
+    if ((xp - x1) * (xp - x2) < 0) and ((xp - x3) * (xp - x4) < 0):
+        return 1
+    else:
+        return 0
+
+def straightLine(x1, y1, x2, y2):
+    x = x1 - x2
+    if x == 0:
+        m = 1e6
+    else:
+        m = (y1 - y2) / x
+    b = y1 - m * x1
+    return m, b
+
+# def slope(x1, y1, x2, y2):
+#     tanx = (y2 - y1) / (x2 - x1)
+#     s = math.atan(tanx)
+#     s = (180 / math.pi) * s
+#     return s
+
+if __name__ == '__main__':
+    R = 71
+    X1, Y1 = 0, 0
+    X2, Y2 = 0, 0
+    CCX, CCY = [0] * 100, [0] * 100
+
+    source = cv2.imread('frames/frame_0008.png')
+    lpf = cv2.imread('medianfiltered/frame_0008.png')
+    output_dir = 'hasil'
+    os.makedirs(output_dir, exist_ok=True)
+    output_image_path = os.path.join(output_dir, f'hasil.png')
+    res = high_boost_filter(source, lpf, 1.5)
+    res = morph(res)
+    res = thresholding(res)
+    res = canny(res)
+    res = region_filter(res)
+
+    height, width = res.shape
+    X1, Y1 = (width // 2), (height // 2)
+    X2, Y2 = (X1 + 22), (Y1+ 23)
+
+    res = coLinear(res)
+    contours, hierarchy = cv2.findContours(res, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    print(contours)
+    print(hierarchy)
+    cv2.imwrite(output_image_path, res)
