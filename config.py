c43b925d58674a32ac825bca2ee17abcb4c4941d
Jovan Josafat <jovan.josafat@gmail.com>
2024-06-12 17:38:41 +0700

    updated

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY", 'pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw')
-    SECURITY_PASSWORD_SALT =  os.environ.get("SECURITY_PASSWORD_SALT", '146585145368132386173505678016728509634')
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECURITY_PASSWORD_SALT =  os.environ.get("SECURITY_PASSWORD_SALT")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = False
     SECURITY_TOKEN_AUTHENTICATION_HEADER = 'Authentication-Token'
