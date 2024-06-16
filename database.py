38abb6bb7d043b61cb5421eb50717d00fdfcdf10
Jovan Josafat <jovan.josafat@gmail.com>
2024-06-16 13:22:12 +0700

    updated

--- database.py
+++ database.py
@@ -5,12 +5,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 
 load_dotenv()
+os.getenv("DB_HOST")
 url_object = URL.create(
     "mysql+pymysql",
     username=os.getenv("DB_USERNAME"),
     password=os.getenv("DB_PASSWORD"),
     host=os.getenv("DB_HOST"),
     database=os.getenv("DB_NAME"),
+    port=os.getenv("DB_PORT")
 )
 engine = create_engine(url_object)
 db_session = scoped_session(sessionmaker(autocommit=False,
