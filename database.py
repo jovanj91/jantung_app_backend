8377927b7c2a3299a999a6b905b8ec6280b5a078
Jovan Josafat <jovan.josafat@gmail.com>
2024-06-16 13:23:22 +0700

    updated

--- database.py
+++ database.py
@@ -5,7 +5,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 
 load_dotenv()
-os.getenv("DB_HOST")
 url_object = URL.create(
     "mysql+pymysql",
     username=os.getenv("DB_USERNAME"),
